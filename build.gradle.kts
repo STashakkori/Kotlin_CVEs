5b1d82274b3895458060049dae677622c22877c5
$t@$h <sinatashakkori@gmail.com>
2023-11-16 22:18:04 -0500

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.9.2")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
